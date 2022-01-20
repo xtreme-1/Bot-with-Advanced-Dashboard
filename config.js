02ab582498e9234569c9f7a426c6737305a9dccb
xtreme-1 <91520321+xtreme-1@users.noreply.github.com>
2022-01-20 10:28:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ module.exports = {
  "description": "",
  "domain": "", // This Domain need to be without https:// or http:// (eg: hydrabot.fun not https://hydrabot.fun)
  "google_analitics": "", 
- "token": "",
+ "token": "OTMyMjc3MjUzMTQ3MDg2ODQ5.YeQo2g.dOe5GZxeYbW6JhdTuIPxHuZi9oI",
  "https":"https://",
  "port":"5003",
- "client_id":"",
- "secret":"",
- "support_server_link":"",
- "bot_name":""
+ "client_id":"932277253147086849",
+ "secret":"2CQ5qalY-QO_RKoCOkxrx-Chg6IRVkrQ",
+ "support_server_link":"https://discord.gg/e8sYf4pHAG",
+ "bot_name":"RoMart Bot"
 }
